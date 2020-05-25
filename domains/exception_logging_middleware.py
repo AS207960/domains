e1a3c055b2a8f6b379e03599c354b4d4ce0bfec2
Q <q@misell.cymru>
2020-05-25 11:59:43 +0100

    exception logging take 2

--- domains/exception_logging_middleware.py
+++ domains/exception_logging_middleware.py
@@ -2,5 +2,8 @@ import logging
 
 
 class ExceptionLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
     def process_exception(self, request, exception):
         logging.exception('Exception handling request for ' + request.path)
