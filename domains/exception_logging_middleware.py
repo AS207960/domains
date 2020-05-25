3d5863386046e15ff3e21fc8464d2a39e7b06935
Q <q@misell.cymru>
2020-05-25 12:11:12 +0100

    fix middlewear

--- domains/exception_logging_middleware.py
+++ domains/exception_logging_middleware.py
@@ -5,5 +5,9 @@ class ExceptionLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
     def process_exception(self, request, exception):
         logging.exception('Exception handling request for ' + request.path)
