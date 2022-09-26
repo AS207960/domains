d37ed1282d4493a4a4b827cf36d88b75b7b81bef
Q <q@misell.cymru>
2022-09-26 20:51:20 +0100

    fix verisign RDAP

--- domains/rdap.py
+++ domains/rdap.py
@@ -184,7 +184,7 @@ class RDAPServicer(rdap_pb2_grpc.RDAPServicer):
 
     def domain_to_proto(self, domain_obj: models.DomainRegistration) -> rdap_pb2.Domain:
         domain_data = apps.epp_client.get_domain(domain_obj.domain)
-        zone_data = zone_info.get_domain_info(domain_data.name)[0]
+        zone_data = zone_info.get_domain_info(domain_data.name.lower())[0]
 
         resp_data = rdap_pb2.Domain(
             handle=domain_data.registry_id,
