14839ebc73aefde2a0c069ef4fded7e23c3d0cc9
Q <q@misell.cymru>
2023-11-11 12:34:20 +0000

    fix permissions for host create

--- domains/views/hosts.py
+++ domains/views/hosts.py
@@ -166,7 +166,7 @@ def host_create(request, host_name: str):
     form = None
 
     domain = None
-    for domain_obj in models.DomainRegistration.get_object_list(access_token, action='create-ns'):
+    for domain_obj in models.DomainRegistration.get_object_list(access_token, action='create-ns-domain'):
         if host_name.lower().endswith(domain_obj.domain):
             domain = domain_obj
             break
