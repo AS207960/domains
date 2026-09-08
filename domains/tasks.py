586d6ea96be9b4b2fb47e511bf37126c43999f0f
Q Misell <q@magicalcodewit.ch>
2026-09-08 19:57:14 +0200

    ignore invalid argument, take 2

--- domains/tasks.py
+++ domains/tasks.py
@@ -982,7 +982,14 @@ def process_domain_transfer_contacts(transfer_order_id):
                 raise rpc_error
 
     if should_send:
-        apps.epp_client.stub.DomainUpdate(update_req)
+        try:
+            apps.epp_client.stub.DomainUpdate(update_req)
+        except grpc.RpcError as rpc_error:
+            error_code = utils.epp_grpc_error_code(rpc_error)
+            if error_code == "invalid-argument":
+                pass
+            else:
+                raise rpc_error
 
 @shared_task(
     autoretry_for=(Exception,), retry_backoff=1, retry_backoff_max=60, max_retries=None, default_retry_delay=3,
